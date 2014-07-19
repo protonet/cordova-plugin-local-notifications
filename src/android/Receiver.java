2a33cd24f5ae2917619a4c3a959a9238cfb32ee6
Christopher Blum <christopher.blum@spellboy.com>
2014-07-19 12:14:14 +0200

    Always vibrate and change led interval

--- src/android/Receiver.java
+++ src/android/Receiver.java
@@ -127,7 +127,8 @@ public class Receiver extends BroadcastReceiver {
             .setLargeIcon(options.getIcon())
             .setAutoCancel(options.getAutoCancel())
             .setOngoing(options.getOngoing())
-            .setLights(options.getColor(), 500, 500);
+            .setVibrate(new long[] { 300, 300, 300, 300, 300 })
+            .setLights(options.getColor(), 3000, 3000);
 
         if (sound != null) {
             notification.setSound(sound);
